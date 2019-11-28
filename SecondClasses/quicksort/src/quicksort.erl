400bc6beeee1929e567a597d0596f751d60ce9ef
fricze <andrzej.fricze@gmail.com>
2019-11-28 22:45:36 +0100

    Fold to filter

--- SecondClasses/quicksort/src/quicksort.erl
+++ SecondClasses/quicksort/src/quicksort.erl
@@ -2,6 +2,15 @@
 
 -export([sort/1]).
 
+
+divide_factory(F) ->
+    fun (El, [S, B]) ->
+            case El < F of
+                true -> [S ++ [El], B];
+                false -> [S, B ++ [El]]
+            end
+    end.
+
 sort([]) -> [];
 sort([A]) -> [A];
 %% sort([F | List]) ->
@@ -9,7 +18,11 @@ sort([A]) -> [A];
 %%     B = sort(lists:filter(fun (X) -> X >= F end, List)),
 %%     S ++ [F] ++ B.
 
+%% sort([F | List]) ->
+%%     S = sort([X || X <- List, X < F]),
+%%     B = sort([X || X <- List, X >= F]),
+%%     S ++ [F] ++ B.
+
 sort([F | List]) ->
-    S = sort([X || X <- List, X < F]),
-    B = sort([X || X <- List, X >= F]),
-    S ++ [F] ++ B.
+    [S, B] = lists:foldl(divide_factory(F), [[], []], List),
+    sort(S) ++ [F] ++ sort(B).
