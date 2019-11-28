1ab8a98fbf5fb5e418b4778960fae5b1c89ec57d
fricze <andrzej.fricze@gmail.com>
2019-11-28 13:31:26 +0100

    Class 3

--- FirstClasses/average/src/average.erl
+++ FirstClasses/average/src/average.erl
@@ -1,6 +1,19 @@
 -module(average).
 
--export([average/1]).
+-export([average/1, sum/1, sum2/1, length2/1]).
+
+sum([]) ->
+    0;
+sum([Hd | Tail]) ->
+    Hd + sum(Tail).
+
+sum2(_List) ->
+    lists:foldl(fun (El, Acc) -> El + Acc end, 0, _List).
+
+length2([]) ->
+    0;
+length2([_ | Tail]) ->
+    1 + length2(Tail).
 
 average(_List) ->
-    ok.
+    sum(_List) / length(_List).
