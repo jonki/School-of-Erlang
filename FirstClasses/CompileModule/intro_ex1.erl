b321e2f48853f0e933e8989d303fd2e7b4e143fe
fricze <andrzej.fricze@gmail.com>
2019-11-28 13:20:02 +0100

    Two classes <3

--- FirstClasses/CompileModule/intro_ex1.erl
+++ FirstClasses/CompileModule/intro_ex1.erl
@@ -1,9 +1,9 @@
 -module(intro_ex1).
 
--export([hello_name/1]).
+-export([hello_name/1, hello_name/2]).
 
 hello_name(Name) ->
     io:fwrite("Hello ~p!\n", [Name]).
 
-hello(Name, Surname) ->
-    io:fwrite("Hello ~p!\n", [Name]).
+hello_name(Name, Surname) ->
+    io:fwrite("Hello ~p ~p!\n", [Name, Surname]).
