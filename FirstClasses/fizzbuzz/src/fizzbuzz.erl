a990eef69c124244412cd19a8fb515257ce9d858
fricze <andrzej.fricze@gmail.com>
2019-11-28 16:58:26 +0100

    Sum of thousand

--- FirstClasses/fizzbuzz/src/fizzbuzz.erl
+++ FirstClasses/fizzbuzz/src/fizzbuzz.erl
@@ -2,5 +2,23 @@
 
 -export([fizzbuzz/0]).
 
+replace(El) ->
+    case [El rem 3, El rem 5] of
+        [0, 0] ->
+            fizzbuzz;
+        [0, _] ->
+            fizz;
+        [_, 0] ->
+            buzz;
+        _Else ->
+            El
+    end.
+
+remainders(El) ->
+    [El rem 3, El rem 5].
+
+replace2(El) ->
+    El.
+
 fizzbuzz() ->
-    ok.
+    lists:map(fun replace/1, lists:seq(1, 100)).
--- FirstClasses/multiples/src/multiples.erl
+++ FirstClasses/multiples/src/multiples.erl
@@ -2,4 +2,20 @@
 
 -export([thousand_sum/0]).
 
-thousand_sum() -> 1.
+thousand() ->
+    lists:seq(1, 1000).
+
+calc(El, Acc) ->
+    case [El rem 3, El rem 5] of
+        [0, 0] ->
+            Acc + El;
+        [0, _] ->
+            Acc + El;
+        [_, 0] ->
+            Acc + El;
+        _Else ->
+            Acc
+    end.
+
+thousand_sum() ->
+    lists:foldl(fun calc/2, 0, thousand()).
