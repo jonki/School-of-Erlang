6f1825f4db7d5cf058825123b503577e5fecabc6
fricze <andrzej.fricze@gmail.com>
2019-11-28 21:59:02 +0100

    I do Erlang

--- SecondClasses/quicksort/src/quicksort.erl
+++ SecondClasses/quicksort/src/quicksort.erl
@@ -2,5 +2,14 @@
 
 -export([sort/1]).
 
-sort(L) ->
-    1.
+sort([]) -> [];
+sort([A]) -> [A];
+%% sort([F | List]) ->
+%%     S = sort(lists:filter(fun (X) -> X < F end, List)),
+%%     B = sort(lists:filter(fun (X) -> X >= F end, List)),
+%%     S ++ [F] ++ B.
+
+sort([F | List]) ->
+    S = sort([X || X <- List, X < F]),
+    B = sort([X || X <- List, X >= F]),
+    S ++ [F] ++ B.
